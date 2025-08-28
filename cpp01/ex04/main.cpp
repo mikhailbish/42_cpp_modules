305c3a5417e19039375c99602d75cc65b9cba634
Mikhail Butuzov <mikhailbish@gmail.com>
2025-08-28 22:26:10 +0300

    sed working, but not bulletproof

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -19,28 +19,62 @@ int main(int argc, char *str[])
 		return (1);
 	int flag = 1;
 	size_t stringPosition = 0;
+	
+	std::string fileName = str[1];
 	std::string textCollector = "";
 	std::string line;
 	std::string textToWrite;
-	std::ifstream fileToRead(str[1]);
-	//fileToRead
-	std::string buff;
-//	fileToRead >> buff
-	while (getline(fileToRead, line))
+	std::string strToFind = str[2];
+	std::string strToAdd = str[3];
+	if (strToFind.length() == 0)
 	{
-		if (!flag)
-			textCollector += "\n";
-		if (flag)
-			flag = 0;
-		textCollector += line;
+		std::cout << "We do not look for empty strings" << std::endl;
+		return (1);
 	}
+	std::ifstream fileToRead(fileName);
+	if (fileToRead.fail())
+	{
+		std::cout << "Cannot open file to read" << std::endl;
+		return (1);
+	}
+	std::ofstream fileToWrite(fileName + ".replace");
+	if (fileToWrite.fail())
+	{
+		std::cout << "Cannot open file to write" << std::endl;
+		fileToRead.close();
+		return (1);
+	}
+	
+	try 
+	{
+		while (getline(fileToRead, line))
+		{
+			if (!flag)
+				textCollector += "\n";
+			if (flag)
+				flag = 0;
+			textCollector += line;
+		}
+		while (stringPosition < textCollector.length())
+		{
+			stringPosition = textCollector.find(strToFind, 0);
+			if (stringPosition == std::string::npos)
+				break ; 
+			textToWrite += textCollector.substr(0, stringPosition);
+			textToWrite += strToAdd;
+			textCollector = textCollector.substr(stringPosition + strToFind.length(), textCollector.length() - stringPosition - strToFind.length());
+		}
+		textToWrite += textCollector;
+		fileToWrite << textToWrite;
+	}
+	catch (...)
+	{
+		fileToRead.close();
+		fileToWrite.close();
+		return (1);
+	}	
 	fileToRead.close();
-	while (stringPosition < textCollector.length())
-	{
-		stringPosition = textCollector.find(, stringPosition)
-//		if ()
-//			flag = 1;
-	}
-	std::cout << textCollector << std::endl;
+	fileToWrite.close();
+	
 	return (0);
 }
