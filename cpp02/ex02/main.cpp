c9980424f58935162ed98e24c7305790a5163922
Mikhail Butuzov <mikhailbish@gmail.com>
2025-09-25 21:01:17 +0300

    test case wip

--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -2,13 +2,116 @@
 #include <iostream>
 
 #include <iomanip>
+
+void testCase(std::string& prompt, bool outcome, std::string& messageFail, float num1, float num2, Fixed fixed1, Fixed fixed2)
+{
+	if (!outcome)
+	{
+		std::cout << prompt << ": " << messageFail << std::endl;
+		std::cout << "Float1: " << num1 << std::endl;
+		std::cout << "Float2: " << num2 << std::endl;
+		std::cout << "Fixed1: " << fixed1 << std::endl;
+		std::cout << "Fixed2: " << fixed2 << std::endl;
+	}
+}
+
+//#define FIXED_MIN -8388608
+//#define FIXED_MAX 8388607
+#define FIXED_MIN -100000
+#define FIXED_MAX 100000
+
+float getFloatMin(float f1, float f2)
+{
+	return ( f1 < f2 ? f1 : f2);
+}
+
+float getFloatMax(float f1, float f2)
+{
+	return ( f1 > f2 ? f1 : f2);
+}
+
+float getMinFloatStep(float num)
+{
+	int fractBits = 8;
+	float step = ((float)1) / (1<<fractBits);
+	while ((num  + step) == num || fractBits != 0)
+	{
+		fractBits -= 1;
+		if(fractBits )
+		step = ((float)1) / (1<<fractBits);
+	}
+	return (step);
+
+}
+
 int main( void ) {
+	std::string promptComparison = "comaparison prompt";
+	std::string messageFail = "message fail";
+	
+
+	
 	Fixed const a(10.5f);
 	Fixed const b( 10.5f );
 	Fixed const c( 42.42f );
 	Fixed const e( -42.42f );
 	Fixed const d( b );
+
+	
+	float num1 = FIXED_MIN;
+	float num2 = FIXED_MAX;
+//	double num1 = FIXED_MIN;
+//	double num2 = FIXED_MAX;
+	Fixed fixed1(num1);
+	Fixed fixed2(num2);
+	float step = getMinFloatStep(num1);
+	
+	while (num1 < FIXED_MAX || fixed1 < FIXED_MAX )
+	{
+		testCase( promptComparison, (num1 < num2) == (fixed1 < fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, (num1 > num2) == (fixed1 > fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, (num1 <= num2) == (fixed1 <= fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, (num1 >= num2) == (fixed1 >= fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, (num1 == num2) == (fixed1 == fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, (num1 != num2) == (fixed1 != fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, getFloatMin(num1, num2) == Fixed::min(fixed1, fixed2).toFloat(), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, getFloatMax(num1, num2) == Fixed::max(fixed1, fixed2).toFloat(), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, Fixed(num1 * num2) == (fixed1 * fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, Fixed(num1 / num2) == (fixed1 / fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, Fixed(num1 - num2) == (fixed1 - fixed2), messageFail, num1, num2, fixed1, fixed2);
+		testCase( promptComparison, Fixed(num1 + num2) == (fixed1 + fixed2), messageFail, num1, num2, fixed1, fixed2);
+		fixed1++;
+		fixed2--;
+		step = getMinFloatStep(num1);
+/*
+		if (num1 + step == num1)
+			fractBits--;
+			num1 += 2 * step;
+		else
+*/
+			num1 +=  step;
+/*		if (num1 + step == num1)
+			num1 -= 2 * step;
+		else
+*/
+			num1 -= step;
+//		num2 -= ((float)1) / (1<<8);
+	
+	}
+
+
+/*
+	num += ((float)1) / (1<<8);
+	j(num);
+	std::cout << "Float: " << num << std::endl;
+	std::cout << "Fixed: " << j << std::endl;
+*/
+	return (0);
+
+
 	Fixed i = 0;
+
+
+//	testCase("test prompt", );
 //	a = Fixed( 1234.4321f );
 	std::cout << ((c >= b) ? "True" : "False") << std::endl;
 	std::cout << ((a > b) ? "True" : "False") << std::endl;
