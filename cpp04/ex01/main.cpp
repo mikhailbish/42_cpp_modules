94d7e0def81fa901cc81338c93735f8c07639943
Mikhail Butuzov <mikhailbish@gmail.com>
2025-10-14 19:06:35 +0300

    cpp04 testing wip

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -24,9 +24,21 @@ int checkBrain(Dog &cat)
 	return (cat.getBrain() != nullptr);
 }
 
+void animalsProcess(Animal **animals)
+{
+	try {
+		for (int i = 0; i < 10; i++)
+			animals[i]->makeSound();
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Error: "  << e.what() << std::endl;
+	}
+}
+
 int main()
 {
-
+/*
 	{
 		Animal animalArray[10];
 		std::cout << "-----------------------------------------------------"<<std::endl;
@@ -41,7 +53,6 @@ int main()
 		std::cout << "-----------------------------------------------------"<<std::endl;
 	}
 
-/*
 	{
 		Animal *animalArray[10];
 		std::cout << "-----------------------------------------------------"<<std::endl;
@@ -61,6 +72,73 @@ int main()
 		
 	}
 */
+/*
+	{
+		Animal *animalPtrArray[10];
+		Animal animalArray[10];
+		(void)animalPtrArray;
+//		Dog dogs[5];
+//		Cat cats[5];
+		for (int i = 0; i < 10; i++)
+		{
+			animalPtrArray[i] = &animalArray[i];
+		}
+//		animalPtrArray = (Animal **)&animalArray;
+		std::cout << "-----------------------------------------------------"<<std::endl;
+		for (int i = 0; i < 10; i++)
+		{
+			if (i % 2 == 0)
+				*animalPtrArray[i] = Dog();
+			else
+				*animalPtrArray[i] = Cat();
+	//		animalArray[i].makeSound();
+		}
+		animalsProcess(animalPtrArray);
+//		for (int i = 0; i < 5; i++)
+//		{
+//			dogs[i].~Dog();
+//			cats[i].~Cat();
+//		}
+//		for (int i = 0; i < 10; i++)
+//			animalArray[i].makeSound();
+		std::cout << "-----------------------------------------------------"<<std::endl;
+	}
+*/
+/*
+	{
+	//	Animal **animalPtrArray;
+		Animal *animalArray[10];
+//		Animal animals[10];
+		for (int i = 0; i < 10; i++)
+		{
+//			animalArray[i] = &animals[i];
+		}
+//		(void)animalPtrArray;
+		Dog dogs[5];
+		Cat cats[5];
+//		animalPtrArray = (Animal **)&animalArray;
+		std::cout << "-----------------------------------------------------"<<std::endl;
+		for (int i = 0; i < 10; i++)
+		{
+			if (i % 2 == 0)
+				animalArray[i] = &dogs[i/2];
+			else
+				animalArray[i] = &cats[i/2];
+		}
+		for (int i = 0; i < 5; i++)
+		{
+//			dogs[i].~Animal();
+//			cats[i].~Animal();
+		}
+		(void)animalArray;
+		for (int i = 0; i < 10; i++)
+			animalArray[i]->makeSound();
+		std::cout << "-----------------------------------------------------"<<std::endl;
+	}
+*/
+
+
+
 /*
 	Cat catA;
 	if (!checkBrain(catA))
