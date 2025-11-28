5f945bbfe4ec14d7882170f826f92affea8800b7
Mikhail Butuzov <mikhailbish@gmail.com>
2025-11-29 01:09:54 +0200

    cpp06ex00 looking done

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -1,100 +1,139 @@
 #include "ScalarConverter.hpp"
 
-//char
-//int
-//float
-//double
-/*
-bool ScalarConverter::_isValidDouble(const std::string &text)
+bool ScalarConverter::_isValidLong(const std::string &text)
 {
 	try {
 		size_t endIdx;
-		double res = stod(text, &endIdx);
-		if (text[endIdx])
+		stol(text, &endIdx);
+		if (!((text[endIdx] == 'f' && text.length() - 1 == endIdx ) ||  !text[endIdx]))
 			return (false);
-		return (true;
+		return (true);
 	}
 	catch (std::exception &e)
 	{
 		return (false);
 	}
 }
-*/
 
-std::string ScalarConverter::_resolvedDouble(const std::string &text)
+bool ScalarConverter::_isValidDouble(const std::string &text)
 {
 	try {
 		size_t endIdx;
-		double res = stod(text, &endIdx);
-		if (text[endIdx])
-			return ("impossible");
-		std::ostringstream str;
-		str << res;
-		return str.str();
+		double doubleNumber = stod(text, &endIdx);
+		long double longDoubleNumber = stold(text, &endIdx);
+		if (!((text[endIdx] == 'f' && text.length() - 1 == endIdx ) ||  !text[endIdx]))
+			return (false);
+		if (static_cast<long double>(doubleNumber) - longDoubleNumber != 0)
+			return (false);
+		return (true);
 	}
 	catch (std::exception &e)
 	{
-		return ("impossible");
+		return (false);
 	}
 }
 
-std::string ScalarConverter::_resolvedInteger(const std::string &text)
+bool ScalarConverter::_isValidFloat(const std::string &text)
+{
+	try {
+		size_t endIdx;
+		float fnum = stof(text, &endIdx);
+		if (!((text[endIdx] == 'f' && text.length() - 1 == endIdx ) ||  !text[endIdx]))
+			return (false);
+		if (!((static_cast<double>(fnum) - stod(text)) == 0))
+			return (false);
+		return (true);
+	}
+	catch (std::exception &e)
+	{
+		return (false);
+	}
+}
+
+bool ScalarConverter::_isValidInt(const std::string &text)
+{
+	try {
+		size_t endIdx;
+		stoi(text, &endIdx, 10);
+		if (!((text[endIdx] == 'f' && text.length() - 1 == endIdx ) ||  !text[endIdx]))
+			return (false);
+		return (true);
+	}
+	catch (std::exception &e)
+	{
+		return (false);
+	}
+}
+
+bool ScalarConverter::_isValidChar(const std::string &text)
 {
 	try {
 		size_t endIdx;
 		int res = stoi(text, &endIdx, 10);
-		if (text[endIdx])
-			return ("impossible");
-		std::ostringstream str;
-		str << res;
-		return str.str();
+		if (!((text[endIdx] == 'f' && text.length() - 1 == endIdx ) ||  !text[endIdx]))
+			return (false);
+		if (res < std::numeric_limits<char>::min() || res > std::numeric_limits<char>::max())
+			return (false);
+		return (true);
 	}
 	catch (std::exception &e)
 	{
-		return ("impossible");
+		return (false);
 	}
 }
-std::string ScalarConverter::_resolvedFloat(const std::string &text)
+
+std::string ScalarConverter::_resolve(double num, bool isValidLong)
 {
-	try {
-		size_t endIdx;
-		float res = stof(text, &endIdx);
-		if (text[endIdx])
-			return ("impossible");
-		std::ostringstream str;
-		str.setf(std::ios_base::fixed);
-		str << std::setprecision(1) << std::fixed << res;
-		return str.str();
-	}
-	catch (std::exception &e)
-	{
-		return ("impossible");
-	}
+	std::ostringstream str;
+	if (isValidLong)
+		str << std::setprecision(1) << std::fixed << num;
+	else
+		str << num;
+	return str.str();
 }
-std::string ScalarConverter::_resolvedChar(const std::string &text)
+
+std::string ScalarConverter::_resolve(float num, bool isValidLong)
 {
-	try {
-		size_t endIdx;
-		int res = stoi(text, &endIdx, 10);
-		if (text[endIdx])
-			return ("impossible");
-		if (res < (int)' ' || res > (int)'~')
-			return ("impossible");
-		std::ostringstream str;
-		str << (char)res;
+	std::ostringstream str;
+	if (isValidLong)
+		str << std::setprecision(1) << std::fixed << num << "f";
+	else
+		str << num << "f";
+	return str.str();
+}
+std::string ScalarConverter::_resolve(int num)
+{
+	std::ostringstream str;
+	str << num;
+	return str.str();
+}
+
+std::string ScalarConverter::_resolve(char num)
+{
+	std::ostringstream str;
+	if (std::isprint(num))
+	{
+		str << "'" << num << "'";
 		return str.str();
 	}
-	catch (std::exception &e)
-	{
-		return ("impossible");
-	}
+	return "Non displayable";
 }
 
 void ScalarConverter::convert(const std::string &text)
 {
-	std::cout << "char: " << _resolvedChar(text) << std::endl;
-	std::cout << "int: " << _resolvedInteger(text) << std::endl;
-//	std::cout.setf(std::ios_base::)
-	std::cout << "float: " << _resolvedFloat(text) << std::endl;
-	std::cout << "double: " << _resolvedDouble(text) << std::endl;
+	std::string	imp 	= "impossible";
+	bool 	validLong	= _isValidLong(text);
+	bool 	validDouble	= _isValidDouble(text);
+	bool	validFloat	= _isValidFloat(text);
+	bool	validInt	= _isValidInt(text);
+	bool	validChar	= _isValidChar(text);
+	double	numDouble	= validDouble	? stod(text)			: 0;
+	float	numFloat	= validFloat	? static_cast<float>(numDouble)	: 0;
+	int	numInt		= validInt	? static_cast<int>(numDouble)	: 0;
+	char	numChar		= validChar	? static_cast<char>(numDouble)	: 0;
+
+	std::cout << "char: "	<< (validChar	? _resolve(numChar)			: imp) << std::endl;
+	std::cout << "int: "	<< (validInt	? _resolve(numInt) 			: imp) << std::endl;
+	std::cout << "float: "	<< (validFloat	? _resolve(numFloat, validLong)		: imp) << std::endl;
+	std::cout << "double: " << (validDouble ? _resolve(numDouble, validLong)	: imp) << std::endl;
 }
--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -6,6 +6,7 @@
 //# include <cstdlib>
 # include <sstream>
 # include <iomanip>
+# include <limits>
 
 
 class ScalarConverter {
@@ -14,16 +15,16 @@ class ScalarConverter {
 		ScalarConverter(ScalarConverter &) = delete;
 		ScalarConverter &operator=(ScalarConverter &) = delete;
 		~ScalarConverter() = delete;
-/*
+
 		static bool _isValidDouble(const std::string &);
 		static bool _isValidFloat(const std::string &);
 		static bool _isValidInt(const std::string &);
 		static bool _isValidChar(const std::string &);
-*/
-		static std::string _resolvedDouble(const std::string &text);
-		static std::string _resolvedInteger(const std::string &text);
-		static std::string _resolvedFloat(const std::string &text);
-		static std::string _resolvedChar(const std::string &text);
+		static bool _isValidLong(const std::string &);
+		static std::string _resolve(double num, bool isValidInt);
+		static std::string _resolve(float num, bool isValidInt);
+		static std::string _resolve(int num);
+		static std::string _resolve(char num);
 	public:
 		static void convert(const std::string &);
 };
