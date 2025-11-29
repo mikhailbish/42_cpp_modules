3bc49d1722e12d37e8d4c208535aa08234a01d81
Mikhail Butuzov <mikhailbish@gmail.com>
2025-11-29 02:51:11 +0200

    cpp06ex02 done

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,88 @@
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include "Base.hpp"
+#include <iostream>
+
+Base *generate(void)
+{
+	int random = rand() % 3;
+	if (random == 0)
+		return new(std::nothrow)A();
+	if (random == 1)
+		return new(std::nothrow)B();
+	return new(std::nothrow)C();
+}
+
+void identify(Base *p)
+{
+	try {
+		if (dynamic_cast<A *>(p))
+			std::cout << "A" << std::endl;
+		if (dynamic_cast<B *>(p))
+			std::cout << "B" << std::endl;
+		if (dynamic_cast<C *>(p))
+			std::cout << "C" << std::endl;
+	} catch (std::exception &e)
+	{
+		std::cout << "Error in identify ptr" << e.what() << std::endl;
+	}
+}
+
+void identify(Base &p)
+{
+	try {
+		A &refA = dynamic_cast<A &>(p);
+		(void)refA;
+		std::cout << "A" << std::endl;
+	} catch (std::exception &e)
+	{}
+	try {
+		B &refB = dynamic_cast<B &>(p);
+		(void)refB;
+		std::cout << "B" << std::endl;
+	} catch (std::exception &e)
+	{}
+	try {
+		C &refC = dynamic_cast<C &>(p);
+		(void)refC;
+		std::cout << "C" << std::endl;
+	} catch (std::exception &e)
+	{}
+}
+
+int main()
+{
+	srand(time(0));
+	Base *ptrA = nullptr;
+	Base *ptrB = nullptr;
+	Base *ptrC = nullptr;
+	
+	try {
+		ptrA = generate();
+		ptrB = generate();
+		ptrC = generate();
+		identify(ptrA);
+		identify(ptrB);
+		identify(ptrC);
+		if (ptrA == nullptr)
+			return (1);
+		if (ptrB == nullptr)
+			return (1);
+		if (ptrC == nullptr)
+			return (1);
+		Base &refA = *ptrA;
+		Base &refB = *ptrB;
+		Base &refC = *ptrC;
+		identify(refA);
+		identify(refB);
+		identify(refC);
+	}
+	catch (std::exception &e)
+	{
+		std::cout << "Error " << e.what() << std::endl;
+	}
+	delete ptrA;
+	delete ptrB;
+	delete ptrC;
+}
